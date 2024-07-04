822b37a278d9863e9a7ffef210383ecf016adad6
guadalupehuergo-dcsg <142525500+guadalupehuergo-dcsg@users.noreply.github.com>
2024-07-03 23:40:49 -0300

    Update instructions.js

--- instructions.js
+++ instructions.js
@@ -1,12 +1,15 @@
-//on the  chrome  with the snippets open the inspector , then open again from there ctr shift I
+//1 - TO UPDATE THE SNIPPETS LIST 
+// In Chrome (the one you want to save the snippets from) with the snippets open the inspector, then open again from there press ctr+shift+I
+//
 let exportSnippets
 InspectorFrontendHost.getPreferences(_ => { exportSnippets = JSON.parse(_['script-snippets']) })
 
 copy(exportSnippets)
 
-//on the new chrome open the inspector , then open again from there ctr shift I
+//2 - TO ADD THE SNIPPETS ON A NEW CHROME INSTANCE
+//In the new chrome open the inspector,  then open again from there press ctr+shift+I
 
-//copy the json and add them into an array 
+//copy the JSON and add them into an array 
 let importSnippets = []
 // add at the end
 InspectorFrontendHost.setPreference("script-snippets", JSON.stringify(importSnippets)); 
